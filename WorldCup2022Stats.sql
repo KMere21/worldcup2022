4bad655bb1c61b26556bc7ff1f11424d364e1862
Kate Meredith <kmere21@gmail.com>
2022-12-12 11:47:39 -0500

    inital table review

--- WorldCup2022Stats.sql
+++ WorldCup2022Stats.sql
@@ -1,3 +1,33 @@
+## Exploring each table
 SELECT *
 FROM player_defense;
 
+SELECT *
+FROM player_gca;
+
+SELECT * 
+FROM player_keepers;
+
+SELECT *
+FROM player_keepersadv;
+
+SELECT * 
+FROM player_misc;
+
+SELECT * 
+FROM player_passing;
+
+SELECT * 
+FROM player_passing_types;
+
+SELECT * 
+FROM player_playingtime;
+
+SELECT * 
+FROM player_possession;
+
+SELECT * 
+FROM player_shooting;
+
+SELECT * 
+FROM player_stats;
